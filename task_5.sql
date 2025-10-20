9f055c43dbbda11419afe4718226278f1b8e1e2c
Tsh3py <1tshepi.mohitshane@gmail.com>
2025-10-21 01:55:00 +0200

    feat:a script that inserts a single row in the table

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,12 @@
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO Customers (customer_id, customer_name,INSERT INTO customer (
+    customer_id, 
+    customer_name, 
+    email, 
+    address
+)
+VALUES (
+    1, 
+    'Cole Baidoo', 
+    'cbaidoo@sandtech.com', 
+    '123 Happiness Ave.'
+);
